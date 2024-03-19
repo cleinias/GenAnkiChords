2e8e5c14ed8ea789e0ad87a72b8b3c7e0c81bb67
Stefano Franchi <stefano.franchi@gmail.com>
2024-03-19 12:59:35 -0500

    reformatted code and improved the  lilypond template

--- chord-generation.py
+++ chord-generation.py
@@ -5,38 +5,37 @@
 # License GPL
 
 # Import the library needed to manipulate and create an Anki deck
-import genanki  
+import genanki
 import csv
 from string import Template
 import html
 
 # Constants
 chordsDatafile = "ChordsData.csv"
-model_id = 1149467492 # randomly generated with import random; random.randrange(1 << 30, 1 << 31)
+model_id = 1149467492  # randomly generated with import random; random.randrange(1 << 30, 1 << 31)
 deck_id = 1393751746  # randomly generated with import random; random.randrange(1 << 30, 1 << 31)
 
 # Variables
-chordsData = [] # Holds all the data about chords, partly read from file and partly generated here
-ankiNotes  = [] # Holds all the notes generated from chordsdata
-ankiFields = [] # Holds the fields names for the anki model
+chordsData = []  # Holds all the data about chords, partly read from file and partly generated here
+ankiNotes = []  # Holds all the notes generated from chordsdata
+ankiFields = []  # Holds the fields names for the anki model
 
-
-# Reading the chords data into a dictionary indexed 
+# Reading the chords data into a dictionary indexed
 # with the field names from the first row of the chords data file
 
 with open(chordsDatafile, newline='') as csvfile:
     # reader = csv.reader(csvfile, delimiter=';', quotechar='"')
     # fields = next(reader)
-    reader = csv.DictReader(csvfile, delimiter=';', quotechar='"', restval = 'pippo', restkey = 'leftOver')
+    reader = csv.DictReader(csvfile, delimiter=';', quotechar='"', restval='pippo', restkey='leftOver')
     for row in reader:
-      chordsData.append(row)
-    fields = chordsData[6].keys()  
+        chordsData.append(row)
+    fields = chordsData[6].keys()
     # # Testing
     # for x, y in zip(fields, list(chordsData[0].values())):
     #   print(x, ' --> ' , y)
     # for x in chordsData:
     #   print('x --> ', len(fields), ' and ', len(list(chordsData[0].values())))
-      # print('Row length = ', len(list(row.values())))
+    # print('Row length = ', len(list(row.values())))
     # print(fields)
 
 ##################################################################################
@@ -61,77 +60,102 @@ with open(chordsDatafile, newline='') as csvfile:
 # }
 # 
 # \relative c'' { %ANKI% }
- 
- 
+
+
 # As the code to be put into the lilypond fields is not easily abstracted, 
 # a different pattern must be provided for every single field.
 # The simple pattern is as follows (illustrated for A7, shell voicing off 3rd):
 #
-# \new GrandStaff <<
-#                 \new Staff \relative { dod'1}
-#                 \new Staff \relative { \clef bass sol,1 }
-#  >>
+# Notice that the durations and octave placements of the pitches must be added,
+# and are voicing-dependents.
 #
-# This structure leads well to the use of the Template class, 
+# \version "2.24.3"
+# \language "italiano"
+#
+# \score {
+#           \new GrandStaff
+#         <<
+#         \new Staff \relative  {dod'1 }
+#         \new Staff \relative {\clef sol1 }
+#
+#         >>
+#         \midi {}
+#         }
+#
+# This structure leads well to the use of the Template class,
 # with only two placeholders needed, trebleClefNotes and bassClefNotes
 
 # Template for all patterns:
-lilypondString = '[lilypond=chords] \\new GrandStaff <<\\new Staff \\relative { $trebleClefNotes} \\new Staff \\relative { $bassClefNotes} >> [/lilypond]'
+lilypondString = '''[lilypond=chords] 
+                     \version "2.24.3"
+                     \language "italiano"
+                     \score {
+                              \new GrandStaff
+                              <<
+                                \new Staff \relative  {$trebleClefNotes}
+                                \new Staff \relative {\clef bass $bassClefNotes  }
+
+                            >>
+                            \midi {}
+                            }
+                     [/lilypond]'''
 lilypondTemplate = Template(lilypondString)
 
 
 # For Rootless voicing off-3rd and off-7th
-tempChordsData = [] # Needed to copy the modified record over
+tempChordsData = []  # Needed to copy the modified record over
 for chordItem in chordsData:
-  # Proceeding in order:
-  # Rootless Voicing off 3rd
-  notes = chordItem['Rootless_V_Off_3rd']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes= notesList[0].lower(), \
-                                               bassClefNotes = notesList[1].lower())
-    # print(chordItem['Name'], ' --> ', lilypondCode)
-    chordItem['Rootless_V_Off_3rd_lilypond']=html.escape(lilypondCode)
-  # Rootless voicing off 7th
-  notes = chordItem['Rootless_V_Off_7th']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
-                                               bassClefNotes=notesList[1].lower())
-    chordItem['Rootless_V_Off_7th_lilypond']=html.escape(lilypondCode)
-  # Guide tone voicing off 3rd
-  notes = chordItem['GuideTones_V_Off_3rd']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes=' , '.join([notesList[1].lower(), notesList[2].lower()]), \
-                                               bassClefNotes=notesList[0].lower())
-    # print(chordItem['Name'], ' --> ', lilypondCode)
-    chordItem['GuideTones_V_Off_3rd_lilypond']=html.escape(lilypondCode)
-  # Guide tone voicing off 7th
-  notes = chordItem['GuideTones_V_Off_7th']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes=' , '.join([notesList[1].lower(), notesList[2].lower()]), \
-                                               bassClefNotes=notesList[0].lower())
-    chordItem['GuideTones_V_Off_7th_lilypond']=html.escape(lilypondCode)
-  # Four notes shell extensions voicing off 3rd
-  notes = chordItem['FourNotesSh_Ext_V_Off_3rd']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
-                                               bassClefNotes=notesList[1].lower())
-    # print(chordItem['Name'], ' --> ', lilypondCode)
-    chordItem['FourNotesSh_Ext_V_Off_3rd_lilypond']=html.escape(lilypondCode)
-  # Four notes shell extensions off 7th
-  notes = chordItem['FourNotesSh_Ext_V_Off_7th']
-  if notes.strip():
-    notesList = notes.split()
-    lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
-                                               bassClefNotes=notesList[1].lower())
-    chordItem['FourNotesSh_Ext_V_Off_7th_lilypond']=html.escape(lilypondCode)    
-  tempChordsData.append(chordItem)
-  # print(chordItem['Name'], ' --> ', chordItem['Rootless_V_Off_3rd'], '-->', chordItem['Rootless_V_Off_3rd_lilypond'], '-->', chordItem['Rootless_V_Off_7th_lilypond'])
-  
+    # Proceeding in order:
+    # Rootless Voicing off 3rd
+    notes = chordItem['Rootless_V_Off_3rd']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
+                                                   bassClefNotes=notesList[1].lower())
+        # print(chordItem['Name'], ' --> ', lilypondCode)
+        chordItem['Rootless_V_Off_3rd_lilypond'] = html.escape(lilypondCode)
+    # Rootless voicing off 7th
+    notes = chordItem['Rootless_V_Off_7th']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
+                                                   bassClefNotes=notesList[1].lower())
+        chordItem['Rootless_V_Off_7th_lilypond'] = html.escape(lilypondCode)
+    # Guide tone voicing off 3rd
+    notes = chordItem['GuideTones_V_Off_3rd']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(
+            trebleClefNotes=' , '.join([notesList[1].lower(), notesList[2].lower()]), \
+            bassClefNotes=notesList[0].lower())
+        # print(chordItem['Name'], ' --> ', lilypondCode)
+        chordItem['GuideTones_V_Off_3rd_lilypond'] = html.escape(lilypondCode)
+    # Guide tone voicing off 7th
+    notes = chordItem['GuideTones_V_Off_7th']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(
+            trebleClefNotes=' , '.join([notesList[1].lower(), notesList[2].lower()]), \
+            bassClefNotes=notesList[0].lower())
+        chordItem['GuideTones_V_Off_7th_lilypond'] = html.escape(lilypondCode)
+    # Four notes shell extensions voicing off 3rd
+    notes = chordItem['FourNotesSh_Ext_V_Off_3rd']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
+                                                   bassClefNotes=notesList[1].lower())
+        # print(chordItem['Name'], ' --> ', lilypondCode)
+        chordItem['FourNotesSh_Ext_V_Off_3rd_lilypond'] = html.escape(lilypondCode)
+    # Four notes shell extensions off 7th
+    notes = chordItem['FourNotesSh_Ext_V_Off_7th']
+    if notes.strip():
+        notesList = notes.split()
+        lilypondCode = lilypondTemplate.substitute(trebleClefNotes=notesList[0].lower(), \
+                                                   bassClefNotes=notesList[1].lower())
+        chordItem['FourNotesSh_Ext_V_Off_7th_lilypond'] = html.escape(lilypondCode)
+    tempChordsData.append(chordItem)
+    # print(chordItem['Name'], ' --> ', chordItem['Rootless_V_Off_3rd'], '-->', chordItem['Rootless_V_Off_3rd_lilypond'], '-->', chordItem['Rootless_V_Off_7th_lilypond'])
+
 chordsData = tempChordsData
 
 #
@@ -174,8 +198,8 @@ chordsData = tempChordsData
 # creating the list of anki fields from the field names
 
 for field in fields:
-  ankiFields.append({ 'name' : field})
-  
+    ankiFields.append({'name': field})
+
 # print(ankiFields)
 
 
@@ -184,33 +208,32 @@ for field in fields:
 # Generating the model, i.e.,  the note type and the templates
 # 
 chord_model = genanki.Model(
-  model_id,
-  'Chords',
-  fields=ankiFields,
+    model_id,
+    'Chords',
+    fields=ankiFields,
     templates=[
         {
-        'name': 'NotesRootless3',
-        'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr> <font size=16>{{Name}}',
-        'afmt': '{{FrontSide}}<hr id="answer">{{Rootless_V_Off_3rd}} <hr><center>{{Rootless_V_Off_3rd_lilypond}}</center>',
+            'name': 'NotesRootless3',
+            'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr> <font size=16>{{Name}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Rootless_V_Off_3rd}} <hr><center>{{Rootless_V_Off_3rd_lilypond}}</center>',
         },
-         {
-        'name': 'NotesRootless7',
-        'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
-        'afmt': '{{FrontSide}}<hr id="answer">{{Rootless_V_Off_7th}}<hr><center>{{Rootless_V_Off_7th_lilypond}}</center>',
-          },
-         {
-          'name' : 'NotesGuideTones3',  
-          'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Lead tones 3-note voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
-          'afmt': '{{FrontSide}}<hr id="answer">{{GuideTones_V_Off_3rd}}<hr><center>{{GuideTones_V_Off_3rd_lilypond}}</center>',
-           },
-         {
-          'name' : 'NotesGuideTones7',  
-          'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
-          'afmt': '{{FrontSide}}<hr id="answer">{{GuideTones_V_Off_7th}}<hr><center>{{GuideTones_V_Off_7th_lilypond}}</center>',
-           },
-         
-        ])
+        {
+            'name': 'NotesRootless7',
+            'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{Rootless_V_Off_7th}}<hr><center>{{Rootless_V_Off_7th_lilypond}}</center>',
+        },
+        {
+            'name': 'NotesGuideTones3',
+            'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Lead tones 3-note voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{GuideTones_V_Off_3rd}}<hr><center>{{GuideTones_V_Off_3rd_lilypond}}</center>',
+        },
+        {
+            'name': 'NotesGuideTones7',
+            'qfmt': '<center><font size=8>Notes in </font><hr> <font size=14>Rootless shell voicing, <br> <bold>off 3rd</bold> for: </font><hr><font size=16>{{Name}}',
+            'afmt': '{{FrontSide}}<hr id="answer">{{GuideTones_V_Off_7th}}<hr><center>{{GuideTones_V_Off_7th_lilypond}}</center>',
+        },
 
+    ])
 
 # i=1
 # for line in zip(fields,list(chordsData[1].values())):
@@ -220,26 +243,24 @@ chord_model = genanki.Model(
 
 # Generating the notes as list
 for row in chordsData:
-  ankiNotes.append(genanki.Note(
-    model=chord_model,
-    fields=list(row.values())))
+    ankiNotes.append(genanki.Note(
+        model=chord_model,
+        fields=list(row.values())))
 # print(ankiNotes[1])
 
 #
 # Creating the ankideck
 #
 ankiChordsDeck = genanki.Deck(
-  deck_id,
-  'Comping Chords')
+    deck_id,
+    'Comping Chords')
 # 
 
 # Adding the notes to the deck
 #
 # 
 for note in ankiNotes:
-  ankiChordsDeck.add_note(note)
+    ankiChordsDeck.add_note(note)
 
 # # Packing the deck and saving it
 genanki.Package(ankiChordsDeck).write_to_file('Comping-Chords.apkg')
-
-
